a27d0ef4bad86f16aba62d27d23a2a9def9b0233
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-10-19 18:34:43 +0200

    fix: pushsaver api wont work

--- bytezo_website/backend.py
+++ bytezo_website/backend.py
@@ -1,22 +1,22 @@
-from urllib.parse import urlencode
-from urllib.request import Request, urlopen
+import niquests
 
 
 def send_push(key: str, name: str, id: str):
-    url = 'https://www.pushsafer.com/api'
-    post_fields = {
-        "t" : f'New Bytezo Message',
-        "m" : f"{name} send a message for Bytezo",
-        "s" : 11,
-        "v" : 3,
-        "i" : 33,
-        "c" : '#FF0000',
-        "d" : 'a',
-        "u" : f'http://192.168.178.80:8000/get_message/{id}',
-        "ut" : 'Open Full Message',
-        "k" : key,
-        }
+    pushsaver_url = "https://www.pushsafer.com/api"
+    server_url = "localhost:8000"
 
-    request = Request(url, urlencode(post_fields).encode())
-    json = urlopen(request).read().decode()
-    print(json)
+    parameters = {
+        "t": "New Bytezo Message",
+        "m": f"{name} send a message for Bytezo",
+        "s": 11,
+        "v": 3,
+        "i": 33,
+        "c": "#FF0000",
+        "d": "a",
+        "u": f"http://{server_url}/get_message/{id}",
+        "ut": "Open Full Message",
+        "k": key,
+    }
+
+    request = niquests.post(pushsaver_url, params=parameters)
+    print(request.text)
