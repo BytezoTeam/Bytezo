a6d82cd914e5e23854607f6418aabfbfeee12049
2mal3 <56305732+2mal3@users.noreply.github.com>
2025-04-07 18:28:30 +0200

    fix: broken docker image

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 chown -R nonroot:nonroot database
-gosu nonroot gunicorn bytezo_website.server:app -w 4 -b 0.0.0.0:8000
+gosu nonroot gunicorn bytezo_website.main:app -w 4 -b 0.0.0.0:8000
